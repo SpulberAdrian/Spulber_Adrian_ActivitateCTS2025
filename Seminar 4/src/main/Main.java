4f074a5ab66ca14bf14c4033b5d28355f4334dd7
Spulber <spulberadrian396@gmail.com>
2025-03-21 11:31:18 +0200

    Simple Factory/Prototype

---
+++ Seminar 4/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import Prototype.Problema2.ARezervare;
+import Prototype.Problema2.Rezervare;
+import SupeTip.Supe;
+import SupeTip.TipuriSupe;
+import fabrici.FabricaSupa;
+
+public class Main {
+    public static void main(String[] args) {
+        FabricaSupa fabrica = new FabricaSupa(200, 10.0);
+        Supe supa = fabrica.getSupa(TipuriSupe.LEGUME, "bors,legume", 20);
+        Supe supa2 = fabrica.getSupa(TipuriSupe.VITA, "vita,apa",30);
+        supa2.afisareSupe();
+
+        ARezervare rezervare = new Rezervare("Daria", 15 ,"21.10.2020","25");
+        System.out.println(rezervare.toString());
+
+    }
+}
